38432ed35ae72f05f3445be5abbe9ee163eca12d
kiki-kanri <a470666@gmail.com>
2024-11-18 23:25:44 +0800

    chore: configure most perfectionist/sort-related rules and remove sort-keys-plus plugin

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import antfu from '@antfu/eslint-config';
 import type { ConfigNames, TypedFlatConfigItem } from '@antfu/eslint-config';
 import type { FlatConfigComposer } from 'eslint-flat-config-utils';
-// @ts-expect-error No Type Definition File.
-import sortKeysPlus from 'eslint-plugin-sort-keys-plus';
+
+const commonPerfectionistSortOptions = Object.freeze({
+	ignoreCase: false,
+	partitionByNewLine: true,
+	type: 'natural',
+});
 
 export function createConfig(environment: 'bun' | 'node' = 'node', options?: Parameters<typeof antfu>[0]): FlatConfigComposer<TypedFlatConfigItem, ConfigNames> {
 	return antfu(
 		{ typescript: true, ...options },
 		{
 			files: ['**/*.{cjs,js,mjs,ts}'],
-			plugins: { 'sort-keys-plus': sortKeysPlus },
 			rules: {
 				'antfu/curly': 'off',
 				'antfu/if-newline': 'off',
 				'antfu/no-top-level-await': 'off',
 				'curly': ['error', 'multi-line'],
 				'max-classes-per-file': ['error', 1],
+				'perfectionist/sort-array-includes': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-enums': ['error', commonPerfectionistSortOptions],
 				'perfectionist/sort-imports': [
 					'error',
 					{
@@ -28,9 +33,16 @@ export function createConfig(environment: 'bun' | 'node' = 'node', options?: Par
 						],
 						ignoreCase: false,
 						internalPattern: ['@/**', '~/**'],
+						type: 'natural',
 					},
 				],
-				'sort-keys-plus/sort-keys': ['error', 'asc', { caseSensitive: true, natural: true }],
+				'perfectionist/sort-interfaces': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-intersection-types': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-object-types': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-objects': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-sets': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-union-types': ['error', commonPerfectionistSortOptions],
+				'perfectionist/sort-variable-declarations': ['error', commonPerfectionistSortOptions],
 				'style/array-bracket-newline': ['error', 'consistent'],
 				'style/array-element-newline': ['error', 'consistent'],
 				'style/arrow-parens': ['error', 'always'],
