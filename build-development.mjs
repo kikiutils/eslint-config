4eb96bc5d0443e252ac434372d5f326dbd1a87f7
kiki-kanri <a470666@gmail.com>
2024-12-13 15:04:25 +0800

    style: format and lint codes

--- build-development.mjs
+++ build-development.mjs
@@ -4,7 +4,11 @@ import { rm } from 'node:fs/promises';
 const outDir = './dist';
 
 (async () => {
-    await rm(outDir, { force: true, recursive: true });
+    await rm(outDir, {
+        force: true,
+        recursive: true,
+    });
+
     await build({
         bundle: false,
         entryPoints: ['./src/**/*.ts'],
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import antfu from '@antfu/eslint-config';
-import type { ConfigNames, TypedFlatConfigItem } from '@antfu/eslint-config';
+import type {
+    ConfigNames,
+    TypedFlatConfigItem,
+} from '@antfu/eslint-config';
 import type { FlatConfigComposer } from 'eslint-flat-config-utils';
 // @ts-expect-error No declare file.
 import tailwindcss from 'eslint-plugin-tailwindcss';
@@ -12,33 +15,140 @@ const commonPerfectionistSortOptions = Object.freeze({
 
 export function createConfig(environment: 'bun' | 'node' = 'node', options?: Parameters<typeof antfu>[0]): FlatConfigComposer<TypedFlatConfigItem, ConfigNames> {
     return antfu(
-        { typescript: true, ...options },
+        {
+            typescript: true,
+            ...options,
+        },
         {
             files: ['**/*.{cjs,js,mjs,ts,vue}'],
             rules: {
                 'antfu/curly': 'off',
                 'antfu/if-newline': 'off',
                 'antfu/no-top-level-await': 'off',
-                'curly': ['error', 'multi-line'],
-                'max-classes-per-file': ['error', 1],
-                'perfectionist/sort-array-includes': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-enums': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-heritage-clauses': ['error', { ignoreCase: false, type: 'natural' }],
+                'curly': [
+                    'error',
+                    'multi-line',
+                ],
+                'max-classes-per-file': [
+                    'error',
+                    1,
+                ],
+                'perfectionist/sort-array-includes': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-enums': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-heritage-clauses': [
+                    'error',
+                    {
+                        ignoreCase: false,
+                        type: 'natural',
+                    },
+                ],
                 'perfectionist/sort-imports': [
                     'error',
                     {
                         environment,
                         groups: [
-                            ['builtin', 'external'],
+                            [
+                                'builtin',
+                                'external',
+                            ],
                             'internal',
                             'parent',
                             'sibling',
                         ],
                         ignoreCase: false,
-                        internalPattern: ['^#.*', '^@/.*', '^~/.*'],
+                        internalPattern: [
+                            '^#.*',
+                            '^@/.*',
+                            '^~/.*',
+                        ],
                         type: 'natural',
                     },
                 ],
+                'perfectionist/sort-interfaces': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-intersection-types': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-maps': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-object-types': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-objects': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-sets': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-switch-case': [
+                    'error',
+                    {
+                        ignoreCase: false,
+                        type: 'natural',
+                    },
+                ],
+                'perfectionist/sort-union-types': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'perfectionist/sort-variable-declarations': [
+                    'error',
+                    commonPerfectionistSortOptions,
+                ],
+                'style/array-bracket-newline': [
+                    'error',
+                    { minItems: 2 },
+                ],
+                'style/array-element-newline': [
+                    'error',
+                    { minItems: 2 },
+                ],
+                'style/arrow-parens': [
+                    'error',
+                    'always',
+                ],
+                'style/brace-style': [
+                    'error',
+                    '1tbs',
+                    { allowSingleLine: false },
+                ],
+                'style/function-call-spacing': [
+                    'error',
+                    'never',
+                ],
+                'style/indent': [
+                    'error',
+                    4,
+                ],
+                'style/member-delimiter-style': [
+                    'error',
+                    {
+                        multiline: {
+                            delimiter: 'semi',
+                            requireLast: true,
+                        },
+                    },
+                ],
+                'style/no-extra-parens': [
+                    'error',
+                    'all',
+                    { nestedBinaryExpressions: false },
+                ],
+                'style/no-extra-semi': 'error',
                 'style/object-curly-newline': [
                     'error',
                     {
@@ -60,31 +170,34 @@ export function createConfig(environment: 'bun' | 'node' = 'node', options?: Par
                         },
                     },
                 ],
-                'perfectionist/sort-interfaces': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-intersection-types': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-maps': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-object-types': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-objects': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-sets': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-switch-case': ['error', { ignoreCase: false, type: 'natural' }],
-                'perfectionist/sort-union-types': ['error', commonPerfectionistSortOptions],
-                'perfectionist/sort-variable-declarations': ['error', commonPerfectionistSortOptions],
-                'style/array-bracket-newline': ['error', { minItems: 2 }],
-                'style/array-element-newline': ['error', { minItems: 2 }],
-                'style/arrow-parens': ['error', 'always'],
-                'style/brace-style': ['error', '1tbs', { allowSingleLine: false }],
-                'style/function-call-spacing': ['error', 'never'],
-                'style/indent': ['error', 4],
-                'style/member-delimiter-style': ['error', { multiline: { delimiter: 'semi', requireLast: true } }],
-                'style/no-extra-parens': ['error', 'all', { nestedBinaryExpressions: false }],
-                'style/no-extra-semi': 'error',
                 'style/padding-line-between-statements': [
                     'error',
-                    { blankLine: 'always', next: ['class', 'enum', 'function'], prev: '*' },
-                    { blankLine: 'always', next: '*', prev: ['class', 'function'] },
+                    {
+                        blankLine: 'always',
+                        next: [
+                            'class',
+                            'enum',
+                            'function',
+                        ],
+                        prev: '*',
+                    },
+                    {
+                        blankLine: 'always',
+                        next: '*',
+                        prev: [
+                            'class',
+                            'function',
+                        ],
+                    },
                 ],
-                'style/semi': ['error', 'always'],
-                'ts/consistent-generic-constructors': ['error', 'constructor'],
+                'style/semi': [
+                    'error',
+                    'always',
+                ],
+                'ts/consistent-generic-constructors': [
+                    'error',
+                    'constructor',
+                ],
                 'ts/no-redeclare': 'off',
             },
         },
@@ -96,18 +209,58 @@ export function createConfig(environment: 'bun' | 'node' = 'node', options?: Par
                 'tailwindcss/enforces-negative-arbitrary-values': 'error',
                 'tailwindcss/enforces-shorthand': 'error',
                 'tailwindcss/no-unnecessary-arbitrary-value': 'error',
-                'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
-                'vue/html-indent': ['error', 4],
-                'vue/max-attributes-per-line': ['error', { multiline: 1, singleline: 1 }],
-                'vue/max-len': ['error', { code: 256, template: 192 }],
+                'vue/block-order': [
+                    'error',
+                    {
+                        order: [
+                            'template',
+                            'script',
+                            'style',
+                        ],
+                    },
+                ],
+                'vue/html-indent': [
+                    'error',
+                    4,
+                ],
+                'vue/max-attributes-per-line': [
+                    'error',
+                    {
+                        multiline: 1,
+                        singleline: 1,
+                    },
+                ],
+                'vue/max-len': [
+                    'error',
+                    {
+                        code: 256,
+                        template: 192,
+                    },
+                ],
                 'vue/no-multi-spaces': 'error',
             },
         },
         {
             files: ['**/.vscode/*.json'],
             rules: {
-                'jsonc/sort-array-values': ['error', { order: { natural: true, type: 'asc' }, pathPattern: '^.*$' }],
-                'jsonc/sort-keys': ['error', 'asc', { caseSensitive: true, natural: true }],
+                'jsonc/sort-array-values': [
+                    'error',
+                    {
+                        order: {
+                            natural: true,
+                            type: 'asc',
+                        },
+                        pathPattern: '^.*$',
+                    },
+                ],
+                'jsonc/sort-keys': [
+                    'error',
+                    'asc',
+                    {
+                        caseSensitive: true,
+                        natural: true,
+                    },
+                ],
             },
         },
     );
