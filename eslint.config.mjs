66346ebd8d7a31ce87e9ec3e1f5ef0863aebb2bf
kiki-kanri <a470666@gmail.com>
2025-09-13 21:28:38 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 // eslint-disable-next-line antfu/no-import-dist
-import { createConfig } from './dist/index.mjs';
+import { createConfig } from './dist/index.js';
 
 export default createConfig('node', { type: 'lib' }).overrideRules({ 'ts/explicit-function-return-type': 'off' });
