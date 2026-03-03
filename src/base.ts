cadff8e58cc4079f648b071f4526c23483855ce7
kiki-kanri <a470666@gmail.com>
2026-03-03 11:36:32 +0800

    refactor!: split vscode json rules to single file

--- src/base.ts
+++ src/base.ts
@@ -291,28 +291,5 @@ export function createBaseConfigs(environment: 'bun' | 'node' = 'node'): TypedFl
                 'ts/no-redeclare': 'off',
             },
         },
-        {
-            files: ['**/.vscode/*.json'],
-            rules: {
-                'jsonc/sort-array-values': [
-                    'error',
-                    {
-                        order: {
-                            natural: true,
-                            type: 'asc',
-                        },
-                        pathPattern: '^.*$',
-                    },
-                ],
-                'jsonc/sort-keys': [
-                    'error',
-                    'asc',
-                    {
-                        caseSensitive: true,
-                        natural: true,
-                    },
-                ],
-            },
-        },
     ];
 }
