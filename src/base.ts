8f1d2bc4596989874cfedf3d1857423e15be93e6
kiki-kanri <a470666@gmail.com>
2026-03-09 11:17:05 +0800

    refactor!: merge `createVsCodeJsonConfigs` into `createBaseConfigs` and split base configs rules files exts

--- src/base.ts
+++ src/base.ts
@@ -1,4 +1,7 @@
-import type { TypedFlatConfigItem } from '@antfu/eslint-config';
+import type {
+    Rules,
+    TypedFlatConfigItem,
+} from '@antfu/eslint-config';
 // @ts-expect-error No declare file.
 import promise from 'eslint-plugin-promise';
 
@@ -9,6 +12,7 @@ const basePerfectionistSortOptions = {
 
 export function createBaseConfigs(environment: 'bun' | 'node' = 'node'): TypedFlatConfigItem[] {
     return [
+        // All files
         {
             rules: {
                 'e18e/ban-dependencies': [
@@ -18,278 +22,25 @@ export function createBaseConfigs(environment: 'bun' | 'node' = 'node'): TypedFl
                 'e18e/prefer-static-regex': ['off'],
             },
         },
+        // JavaScript
         {
-            files: ['**/*.{cjs,js,mjs,ts,tsx}'],
+            files: ['**/*.{cjs,js,mjs}'],
+            plugins: { promise },
             rules: {
+                ...createBaseRules(environment),
                 'node/prefer-global/process': [
                     'error',
                     'always',
                 ],
             },
         },
+        // TypeScript
         {
-            files: ['**/*.{cjs,js,mjs,ts,tsx,vue}'],
+            files: ['**/*.{ts,tsx}'],
             plugins: { promise },
             rules: {
-                'antfu/consistent-list-newline': [
-                    'error',
-                    {
-                        ArrayExpression: false,
-                        ArrayPattern: false,
-                    },
-                ],
-                'antfu/curly': 'off',
-                'antfu/if-newline': 'off',
-                'antfu/no-top-level-await': 'off',
-                'curly': [
-                    'error',
-                    'multi-line',
-                ],
-                'max-classes-per-file': [
-                    'error',
-                    1,
-                ],
-                'no-promise-executor-return': [
-                    'error',
-                    { allowVoid: true },
-                ],
-                'perfectionist/sort-array-includes': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-enums': [
-                    'error',
-                    {
-                        ...basePerfectionistSortOptions,
-                        sortByValue: 'never',
-                    },
-                ],
-                'perfectionist/sort-heritage-clauses': [
-                    'error',
-                    {
-                        ignoreCase: false,
-                        type: 'natural',
-                    },
-                ],
-                'perfectionist/sort-imports': [
-                    'error',
-                    {
-                        environment,
-                        groups: [
-                            'side-effect',
-                            'side-effect-style',
-                            'style',
-                            [
-                                'value-builtin',
-                                'type-builtin',
-                            ],
-                            [
-                                'value-external',
-                                'type-external',
-                            ],
-                            [
-                                'value-internal',
-                                'type-internal',
-                            ],
-                            [
-                                'value-parent',
-                                'type-parent',
-                            ],
-                            [
-                                'value-sibling',
-                                'type-sibling',
-                            ],
-                            [
-                                'value-index',
-                                'type-index',
-                            ],
-                            'unknown',
-                        ],
-                        ignoreCase: false,
-                        internalPattern: [
-                            '^#.*',
-                            '^@/.*',
-                            '^~/.*',
-                        ],
-                        type: 'natural',
-                    },
-                ],
-                'perfectionist/sort-interfaces': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-intersection-types': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-maps': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-modules': [
-                    'error',
-                    {
-                        ...basePerfectionistSortOptions,
-                        groups: [
-                            'declare-type',
-                            [
-                                'export-type',
-                                'type',
-                            ],
-                            'declare-interface',
-                            [
-                                'export-interface',
-                                'interface',
-                            ],
-                            'declare-enum',
-                            [
-                                'enum',
-                                'export-enum',
-                            ],
-                            'declare-class',
-                            [
-                                'class',
-                                'export-class',
-                            ],
-                            'declare-function',
-                            [
-                                'export-function',
-                                'function',
-                            ],
-                        ],
-                    },
-                ],
-                'perfectionist/sort-object-types': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-objects': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-sets': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-switch-case': [
-                    'error',
-                    {
-                        ignoreCase: false,
-                        type: 'natural',
-                    },
-                ],
-                'perfectionist/sort-union-types': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'perfectionist/sort-variable-declarations': [
-                    'error',
-                    basePerfectionistSortOptions,
-                ],
-                'promise/no-multiple-resolved': 'error',
-                'promise/no-return-in-finally': 'error',
-                'promise/no-return-wrap': 'error',
-                'require-await': 'error',
-                'style/array-bracket-newline': [
-                    'error',
-                    { multiline: true },
-                ],
-                'style/array-element-newline': [
-                    'error',
-                    {
-                        ArrayExpression: {
-                            consistent: true,
-                            minItems: 2,
-                        },
-                        ArrayPattern: { minItems: 3 },
-                    },
-                ],
-                'style/arrow-parens': [
-                    'error',
-                    'always',
-                ],
-                'style/brace-style': [
-                    'error',
-                    '1tbs',
-                    { allowSingleLine: false },
-                ],
-                'style/function-call-spacing': [
-                    'error',
-                    'never',
-                ],
-                'style/indent': [
-                    'error',
-                    4,
-                ],
-                'style/max-len': [
-                    'warn',
-                    {
-                        code: 120,
-                        comments: 120,
-                    },
-                ],
-                'style/member-delimiter-style': [
-                    'error',
-                    {
-                        multiline: {
-                            delimiter: 'semi',
-                            requireLast: true,
-                        },
-                    },
-                ],
-                'style/no-extra-parens': [
-                    'error',
-                    'all',
-                    { nestedBinaryExpressions: false },
-                ],
-                'style/no-extra-semi': 'error',
-                'style/object-curly-newline': [
-                    'error',
-                    {
-                        ExportDeclaration: {
-                            minProperties: 2,
-                            multiline: true,
-                        },
-                        ImportDeclaration: {
-                            minProperties: 2,
-                            multiline: true,
-                        },
-                        ObjectExpression: {
-                            minProperties: 2,
-                            multiline: true,
-                        },
-                        ObjectPattern: {
-                            minProperties: 3,
-                            multiline: true,
-                        },
-                    },
-                ],
-                'style/operator-linebreak': [
-                    'error',
-                    'before',
-                    { overrides: { '=': 'after' } },
-                ],
-                'style/padding-line-between-statements': [
-                    'error',
-                    {
-                        blankLine: 'always',
-                        next: [
-                            'class',
-                            'enum',
-                            'function',
-                        ],
-                        prev: '*',
-                    },
-                    {
-                        blankLine: 'always',
-                        next: '*',
-                        prev: [
-                            'class',
-                            'function',
-                        ],
-                    },
-                ],
-                'style/semi': [
+                ...createBaseRules(environment),
+                'node/prefer-global/process': [
                     'error',
                     'always',
                 ],
@@ -300,5 +51,309 @@ export function createBaseConfigs(environment: 'bun' | 'node' = 'node'): TypedFl
                 'ts/no-redeclare': 'off',
             },
         },
+        // VsCode jsonc
+        {
+            files: ['**/.vscode/*.json'],
+            rules: {
+                'jsonc/sort-array-values': [
+                    'error',
+                    {
+                        order: {
+                            natural: true,
+                            type: 'asc',
+                        },
+                        pathPattern: '^.*$',
+                    },
+                ],
+                'jsonc/sort-keys': [
+                    'error',
+                    'asc',
+                    {
+                        caseSensitive: true,
+                        natural: true,
+                    },
+                ],
+            },
+        },
+        // Vue
+        {
+            files: ['**/*.{vue}'],
+            plugins: { promise },
+            rules: {
+                ...createBaseRules(environment),
+                'ts/consistent-generic-constructors': [
+                    'error',
+                    'constructor',
+                ],
+                'ts/no-redeclare': 'off',
+            },
+        },
     ];
+}
+
+function createBaseRules(environment: 'bun' | 'node' = 'node'): Rules {
+    return {
+        'antfu/consistent-list-newline': [
+            'error',
+            {
+                ArrayExpression: false,
+                ArrayPattern: false,
+            },
+        ],
+        'antfu/curly': 'off',
+        'antfu/if-newline': 'off',
+        'antfu/no-top-level-await': 'off',
+        'curly': [
+            'error',
+            'multi-line',
+        ],
+        'max-classes-per-file': [
+            'error',
+            1,
+        ],
+        'no-promise-executor-return': [
+            'error',
+            { allowVoid: true },
+        ],
+        'perfectionist/sort-array-includes': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-enums': [
+            'error',
+            {
+                ...basePerfectionistSortOptions,
+                sortByValue: 'never',
+            },
+        ],
+        'perfectionist/sort-heritage-clauses': [
+            'error',
+            {
+                ignoreCase: false,
+                type: 'natural',
+            },
+        ],
+        'perfectionist/sort-imports': [
+            'error',
+            {
+                environment,
+                groups: [
+                    'side-effect',
+                    'side-effect-style',
+                    'style',
+                    [
+                        'value-builtin',
+                        'type-builtin',
+                    ],
+                    [
+                        'value-external',
+                        'type-external',
+                    ],
+                    [
+                        'value-internal',
+                        'type-internal',
+                    ],
+                    [
+                        'value-parent',
+                        'type-parent',
+                    ],
+                    [
+                        'value-sibling',
+                        'type-sibling',
+                    ],
+                    [
+                        'value-index',
+                        'type-index',
+                    ],
+                    'unknown',
+                ],
+                ignoreCase: false,
+                internalPattern: [
+                    '^#.*',
+                    '^@/.*',
+                    '^~/.*',
+                ],
+                type: 'natural',
+            },
+        ],
+        'perfectionist/sort-interfaces': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-intersection-types': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-maps': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-modules': [
+            'error',
+            {
+                ...basePerfectionistSortOptions,
+                groups: [
+                    'declare-type',
+                    [
+                        'export-type',
+                        'type',
+                    ],
+                    'declare-interface',
+                    [
+                        'export-interface',
+                        'interface',
+                    ],
+                    'declare-enum',
+                    [
+                        'enum',
+                        'export-enum',
+                    ],
+                    'declare-class',
+                    [
+                        'class',
+                        'export-class',
+                    ],
+                    'declare-function',
+                    [
+                        'export-function',
+                        'function',
+                    ],
+                ],
+            },
+        ],
+        'perfectionist/sort-object-types': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-objects': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-sets': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-switch-case': [
+            'error',
+            {
+                ignoreCase: false,
+                type: 'natural',
+            },
+        ],
+        'perfectionist/sort-union-types': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'perfectionist/sort-variable-declarations': [
+            'error',
+            basePerfectionistSortOptions,
+        ],
+        'promise/no-multiple-resolved': 'error',
+        'promise/no-return-in-finally': 'error',
+        'promise/no-return-wrap': 'error',
+        'require-await': 'error',
+        'style/array-bracket-newline': [
+            'error',
+            { multiline: true },
+        ],
+        'style/array-element-newline': [
+            'error',
+            {
+                ArrayExpression: {
+                    consistent: true,
+                    minItems: 2,
+                },
+                ArrayPattern: { minItems: 3 },
+            },
+        ],
+        'style/arrow-parens': [
+            'error',
+            'always',
+        ],
+        'style/brace-style': [
+            'error',
+            '1tbs',
+            { allowSingleLine: false },
+        ],
+        'style/function-call-spacing': [
+            'error',
+            'never',
+        ],
+        'style/indent': [
+            'error',
+            4,
+        ],
+        'style/max-len': [
+            'warn',
+            {
+                code: 120,
+                comments: 120,
+            },
+        ],
+        'style/member-delimiter-style': [
+            'error',
+            {
+                multiline: {
+                    delimiter: 'semi',
+                    requireLast: true,
+                },
+            },
+        ],
+        'style/no-extra-parens': [
+            'error',
+            'all',
+            { nestedBinaryExpressions: false },
+        ],
+        'style/no-extra-semi': 'error',
+        'style/object-curly-newline': [
+            'error',
+            {
+                ExportDeclaration: {
+                    minProperties: 2,
+                    multiline: true,
+                },
+                ImportDeclaration: {
+                    minProperties: 2,
+                    multiline: true,
+                },
+                ObjectExpression: {
+                    minProperties: 2,
+                    multiline: true,
+                },
+                ObjectPattern: {
+                    minProperties: 3,
+                    multiline: true,
+                },
+            },
+        ],
+        'style/operator-linebreak': [
+            'error',
+            'before',
+            { overrides: { '=': 'after' } },
+        ],
+        'style/padding-line-between-statements': [
+            'error',
+            {
+                blankLine: 'always',
+                next: [
+                    'class',
+                    'enum',
+                    'function',
+                ],
+                prev: '*',
+            },
+            {
+                blankLine: 'always',
+                next: '*',
+                prev: [
+                    'class',
+                    'function',
+                ],
+            },
+        ],
+        'style/semi': [
+            'error',
+            'always',
+        ],
+    };
 }
